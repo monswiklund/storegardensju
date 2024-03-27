79c64f4f1f4f93818fd31597bd31992038d4f433
monswiklund <monswiklund99.studier@gmail.com>
2024-03-27 09:21:23 +0100

    Paddel Match Skapare

---
+++ script/schedule.js
@@ -0,0 +1,36 @@
+class Match {
+  constructor(team1, team2) {
+    this.team1 = team1;
+    this.team2 = team2;
+  }
+}
+
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Schedule {
+  constructor() {
+    this.matches = [];
+  }
+
+  addMatch(match) {
+    this.matches.push(match);
+  }
+
+  generateSchedule(players) {
+    if (players.length % 4 !== 0) {
+      throw new Error("Antalet spelare måste vara en multipel av 4");
+    }
+
+    for (let i = 0; i < players.length; i += 4) {
+      const match = new Match(
+        [players[i], players[i + 1]],
+        [players[i + 2], players[i + 3]]
+      );
+      this.addMatch(match);
+    }
+  }
+}
